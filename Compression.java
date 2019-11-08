fe8d67aa52e46786f0c3cca0d91ebb540b4139da
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2019-11-09 01:10:24 +0530

    Create Compression.java
    Added Code for Compression

---
+++ Compression.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import javax.imageio.stream.ImageOutputStream;
+class Compression {
+ public static void main(String[] args) throws IOException {
+ File input = new File("test.jpg");
+ BufferedImage image = ImageIO.read(input);
+ File compressedImageFile = new File("compress.jpg");
+ OutputStream os = new FileOutputStream(compressedImageFile);
+ Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+ ImageWriter writer = (ImageWriter) writers.next();
+  ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+ writer.setOutput(ios);
+ ImageWriteParam param = writer.getDefaultWriteParam();
+ param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+ param.setCompressionQuality(0.05f);
+ writer.write(null, new IIOImage(image, null, null), param);
+ os.close();
+ ios.close();
+ writer.dispose();
+ }
+}
